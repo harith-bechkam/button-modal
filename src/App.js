5e3f15c770c78503f8d9dd9b64b282916509cd1a
harith-bechkam <harith.kv47@gmail.com>
2021-04-26 12:33:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,23 +4,40 @@ import Modal from "react-modal";
 
  export default function App(){
 const[modalIsOpen,setModalIsOpen]=useState(false);
-const[data,setData]=useState(null);
-const[print,setPrint]=useState(false);
+   const[data,setData]=useState(null);
+//const[print,setPrint]=useState(false);
 
  function getData(val){
 setData(val.target.value)
-setPrint(true)
+//setPrint(true)
 console.log(val.target.value)
+}
+function openbutton(){
+  setData("")
+  setModalIsOpen(true)
+
 }
 function closebutton(){
   {
-    setData("")
-    print?
-    setModalIsOpen(false)
+  
+    //print?
+    //setModalIsOpen(false)
    
     
-    : alert("'Please enter input' if it is empty.")
-    setPrint(false)
+    //: alert("'Please enter input' if it is empty.")
+    //setPrint(false)
+
+ // console.log(data);
+    //(data === nul)?setModalIsOpen(true):setModalIsOpen(fals
+if(data=="")
+{
+setModalIsOpen(true);
+alert("hi");
+}
+else{
+  setModalIsOpen(false)
+}
+//data==""?"hcf":"false"
   }
     
     
@@ -30,7 +47,7 @@ function closebutton(){
 return (
   
     <div className='App'>
-      <button onClick={() => setModalIsOpen(true)}>open</button>
+      <button onClick={openbutton}>open</button>
          <Modal isOpen={modalIsOpen}>
            <label>Input:</label>
             <input id="input" type="text" onChange={getData} autoFocus/>          
